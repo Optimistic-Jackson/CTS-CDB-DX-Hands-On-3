c7a2305d13517700c7c5108e0833944504724c33
Jackson <75530924+Optimistic-Jackson@users.noreply.github.com>
2022-05-02 13:46:30 +0530

    Add files via upload

---
+++ Hands On 3/Hands On 3/main.js
@@ -0,0 +1,251 @@
+let status = false;
+//
+let arr = [
+  {
+    Name: "Jackson",
+    ID: 15678,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn2.vectorstock.com/i/thumb-large/46/76/person-gray-photo-placeholder-man-material-design-vector-23804676.jpg",
+  },
+  {
+    Name: "Jerickson",
+    ID: 15679,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn2.vectorstock.com/i/thumb-large/46/76/person-gray-photo-placeholder-man-material-design-vector-23804676.jpg",
+  },
+  {
+    Name: "Akash",
+    ID: 15680,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn3.vectorstock.com/i/thumb-large/55/92/person-gray-photo-placeholder-woman-vector-24715592.jpg",
+  },
+  {
+    Name: "Sabarish",
+    ID: 15681,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn2.vectorstock.com/i/thumb-large/46/76/person-gray-photo-placeholder-man-material-design-vector-23804676.jpg",
+  },
+  {
+    Name: "Gideon",
+    ID: 15682,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn3.vectorstock.com/i/thumb-large/55/92/person-gray-photo-placeholder-woman-vector-24715592.jpg",
+  },
+  {
+    Name: "Samson",
+    ID: 15683,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn2.vectorstock.com/i/thumb-large/46/76/person-gray-photo-placeholder-man-material-design-vector-23804676.jpg",
+  },
+  {
+    Name: "Robinson",
+    ID: 15684,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn3.vectorstock.com/i/thumb-large/55/92/person-gray-photo-placeholder-woman-vector-24715592.jpg",
+  },
+  {
+    Name: "Parthiban",
+    ID: 15685,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn2.vectorstock.com/i/thumb-large/46/76/person-gray-photo-placeholder-man-material-design-vector-23804676.jpg",
+  },
+  {
+    Name: "Ragland",
+    ID: 15686,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn3.vectorstock.com/i/thumb-large/55/92/person-gray-photo-placeholder-woman-vector-24715592.jpg",
+  },
+  {
+    Name: "Joel",
+    ID: 15687,
+    Skills: "ReactJS, Angular, JS",
+    Project: "Cognizant Internal",
+    HCM: "Suresh",
+    placeholder:
+      "https://cdn2.vectorstock.com/i/thumb-large/46/76/person-gray-photo-placeholder-man-material-design-vector-23804676.jpg",
+  },
+];
+
+myfunc();
+
+//---------------------------------------FUNCTION FOR EDITING THE ROW THING----------------
+function edit(ind) {
+  let x = document.getElementById("skil" + ind);
+  let y = document.getElementById("ed" + ind);
+  x.innerHTML = '<input type="text" id="entered' + ind + '">';
+  y.innerHTML = '<i class="far fa-save" onclick=save(' + ind + ")></i>";
+}
+
+//------------------------------------------FUNCTION FOR SAVING THE ROW-----------------
+function save(ind) {
+  let data = document.getElementById("entered" + ind).value;
+  let x = document.getElementById("skil" + ind);
+  x.innerHTML = data;
+  let y = document.getElementById("ed" + ind);
+  let z = document.getElementById("skils" + ind);
+  z.innerHTML = "Skills: " + data;
+  y.innerHTML = '<i class="far fa-edit" onclick="edit(' + ind + ')"></i>';
+}
+let tab = document.getElementById("mytable");
+//-----------------------FUNCTION FOR DELETING THE ROW--------------
+function odelete(ind) {
+  arr.splice(ind, 1);
+  let x = document.querySelectorAll("tr");
+  x[ind + 1].style.display = "none";
+  console.log(arr);
+  odeletec(ind);
+}
+//-----------------------FUNCTION FOR DELETING THE CARD-----------
+function odeletec(ind) {
+  let x = document.getElementById("crd" + ind);
+  x.style.display = "none";
+  odelete(ind);
+}
+//------------------------FUNCTION FOR SAVING THE CARD--------------------
+function savec(ind) {
+  let data = document.getElementById("enteredc" + ind).value;
+  let z = document.getElementById("skilc" + ind);
+  z.innerHTML = "Skills: " + data;
+  let x = document.getElementById("skil" + ind);
+  x.innerHTML = data;
+}
+//-----------------------------FUNCTION FOR EDITING THE CARD---------------
+function editc(ind) {
+  let x = document.getElementById("skilc" + ind);
+  x.innerHTML =
+    '<label for="enteredc' +
+    ind +
+    '">Skills:</label><input type="text" id="enteredc' +
+    ind +
+    '">';
+}
+
+for (let obj of arr) {
+  let row = tab.insertRow();
+  let cell1 = row.insertCell();
+  let cell2 = row.insertCell();
+  let cell3 = row.insertCell();
+  cell3.id = "skil" + arr.indexOf(obj);
+  let cell4 = row.insertCell();
+  let cell5 = row.insertCell();
+  let cell6 = row.insertCell();
+  let cell7 = row.insertCell();
+
+  cell1.innerHTML = obj.Name;
+  cell2.innerHTML = obj.ID;
+  cell3.innerHTML = obj.Skills;
+  cell3.className = "editable";
+  cell4.innerHTML = obj.Project;
+  cell5.innerHTML = obj.HCM;
+  cell6.innerHTML =
+    '<i class="far fa-edit" onclick="edit(' + arr.indexOf(obj) + ')"></i>';
+  cell6.id = "ed" + arr.indexOf(obj);
+  cell7.innerHTML =
+    '<i class="far fa-minus-square" onclick="odelete(' +
+    arr.indexOf(obj) +
+    ')"></i>';
+}
+
+for (let obj of arr) {
+  let coll = document.createElement("div");
+  coll.className = "col";
+  coll.id = "crd" + arr.indexOf(obj);
+  let el = document.createElement("div");
+  el.className = "card shadow m-5";
+
+  let elh = document.createElement("div");
+  elh.className = "card-header bg-dark";
+  el.appendChild(elh);
+  elh.innerHTML =
+    '<i class="fas fa-times" style="color:red;float:right;font-size:30px" onclick="odeletec(' +
+    arr.indexOf(obj) +
+    ')"></i>';
+
+  let el2 = document.createElement("div");
+  el2.className = "card-body text-center";
+  el.appendChild(el2);
+  let imgg = document.createElement("img");
+  imgg.src = obj.placeholder;
+  imgg.style.width = "100%";
+  el2.appendChild(imgg);
+  let pname = document.createElement("p");
+  pname.innerHTML = "Name: " + obj.Name;
+  el2.appendChild(pname);
+  let pid = document.createElement("p");
+  pid.innerHTML = "ID: " + obj.ID;
+  pname.style.marginTop = "20px";
+  el2.appendChild(pid);
+  let pSkills = document.createElement("p");
+  pSkills.innerHTML = "Skills: " + obj.Skills;
+  pSkills.id = "skilc" + arr.indexOf(obj);
+  el2.appendChild(pSkills);
+  let pProject = document.createElement("p");
+  pProject.innerHTML = "Project:" + obj.Project;
+  el2.appendChild(pProject);
+  let phcm = document.createElement("p");
+  phcm.innerHTML = "HCM: " + obj.HCM;
+  el2.appendChild(phcm);
+  let elf = document.createElement("div");
+  el.appendChild(elf);
+  elf.innerHTML =
+    '<button class="btn btn-success" style="margin-bottom:10px;margin-left:50px" onclick="editc(' +
+    arr.indexOf(obj) +
+    ')">Edit</button> <button class="btn btn-primary" style="margin-bottom:10px;float:right;margin-right:50px" onclick="savec(' +
+    arr.indexOf(obj) +
+    ')">Save</button>';
+  elf.id = "but" + arr.indexOf(obj);
+
+  coll.appendChild(el);
+  let p = document.getElementById("smc");
+  p.appendChild(coll);
+}
+
+function myfunc(arr) {
+  status = !status;
+  if (!status) {
+    let x = document.getElementById("smc");
+    let y = document.getElementById("con");
+    let a = document.getElementById("grid");
+    let b = document.getElementById("tableveiw");
+    x.style.display = "none";
+    y.style.display = "block";
+    a.style.display = "block";
+    b.style.display = "none";
+  } else {
+    let x = document.getElementById("smc");
+    let y = document.getElementById("con");
+    let a = document.getElementById("grid");
+    let b = document.getElementById("tableveiw");
+    x.style.display = "flex";
+    y.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "block";
+  }
+}
